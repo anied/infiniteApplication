cab764eda19495a5115a27b4391fdf386043c06f
Alexander Nied <alex.nied@gmail.com>
2018-12-08 01:33:21 -0600

    Soft-- some new tests. Possible issues. Bedtime.

--- infiniteApplication.js
+++ infiniteApplication.js
@@ -4,6 +4,14 @@ function infiniteApplication(fn, useConfigForArgs, ...initialArgs) {
 		throw new Error('infiniteApplication expects to be called with a function as the first argument.');
 	}
 
+	if (typeof useConfigForArgs === 'undefined') {
+		useConfigForArgs = false;
+	}
+
+	if (typeof useConfigForArgs !== 'boolean') {
+		throw new Error('infiniteApplication expects that a second argument, if present, be a boolean.');
+	}
+
 	let cachedArgs;
 	if (useConfigForArgs) {
 		cachedArgs = Object.assign({}, initialArgs[0]);
--- infiniteApplication.test.js
+++ infiniteApplication.test.js
@@ -14,12 +14,14 @@ describe('infiniteApplication', () => {
 			expect(() => {infiniteApplication()}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
 		});
 		test('It throws an error when being with anything other than a function as the first arg', () => {
-			expect(() => {infiniteApplication(null)}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
-			expect(() => {infiniteApplication(3)}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
-			expect(() => {infiniteApplication('test')}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
-			expect(() => {infiniteApplication({hello: 'world'})}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
-			expect(() => {infiniteApplication(['hello', 'world'])}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
-			expect(() => {infiniteApplication(new Date())}).toThrowError(/^infiniteApplication expects to be called with a function as the first argument.$/);
+			const missingFnError = /^infiniteApplication expects to be called with a function as the first argument.$/;
+			expect(() => {infiniteApplication(null)}).toThrowError(missingFnError);
+			expect(() => {infiniteApplication(true)}).toThrowError(missingFnError);
+			expect(() => {infiniteApplication(3)}).toThrowError(missingFnError);
+			expect(() => {infiniteApplication('test')}).toThrowError(missingFnError);
+			expect(() => {infiniteApplication({hello: 'world'})}).toThrowError(missingFnError);
+			expect(() => {infiniteApplication(['hello', 'world'])}).toThrowError(missingFnError);
+			expect(() => {infiniteApplication(new Date())}).toThrowError(missingFnError);
 		});
 		test('It does not throw an error when called with a function', () => {
 			expect(() => { infiniteApplication(testFn) }).not.toThrowError();
@@ -28,10 +30,25 @@ describe('infiniteApplication', () => {
 			const wrappedFn = infiniteApplication(testFn);
 			expect(typeof wrappedFn).toBe('function');
 		});
+		test('If the second arg is not `undefined` or a boolean it will throw an error', () => {
+			const badSecondArgError = /^infiniteApplication expects that a second argument, if present, be a boolean.$/;
+			expect(() => {infiniteApplication(testFn, null)}).toThrowError(badSecondArgError);
+			expect(() => {infiniteApplication(testFn, 3)}).toThrowError(badSecondArgError);
+			expect(() => {infiniteApplication(testFn, 'test')}).toThrowError(badSecondArgError);
+			expect(() => {infiniteApplication(testFn, {hello: 'world'})}).toThrowError(badSecondArgError);
+			expect(() => {infiniteApplication(testFn, ['hello', 'world'])}).toThrowError(badSecondArgError);
+			expect(() => {infiniteApplication(testFn, new Date())}).toThrowError(badSecondArgError);
+		});
+		test('It does not throw an error when the second arg is a boolean or undefined', () => {
+			expect(() => {infiniteApplication(testFn)}).not.toThrowError();
+			expect(() => {infiniteApplication(testFn, true)}).not.toThrowError();
+		});
 	})
-	xdescribe('The wrapped function', () => {
-		xdescribe('For plain args', () => {});
-		xdescribe('For config args', () => {});
+	describe('The wrapped function', () => {
 		xdescribe('Common functionality to both plain and config arg versions', () => {});
+		xdescribe('For plain args (called w/ no second arg)', () => {
+			// expect('Each time it is called with args, they are appended')
+		});
+		xdescribe('For config args', () => {});
 	});
 });
